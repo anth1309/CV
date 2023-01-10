f3c7ec8c198c25f905cf92820f79cae6aeff54f8
Anthony CHAMBON <acdf-ac.cdf@hotmail.fr>
2023-01-10 09:42:14 +0100

    Modificationmanque media queries

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
   document;
 
-  const professionnalTitle = document.querySelector("#professionnalTitle");
-  const returnAcceuil = document.querySelector(".return");
-  const container = document.querySelector(".professional_experience");
+  const professionnalTitle = document.getElementById("professionnalTitle");
+  const profilTitle = document.getElementById("profilTitle");
+  const formationTitle = document.getElementById("formationTitle");
 
-  returnAcceuil.addEventListener("click", (e) => {
-    e.preventDefault();
-    container.classList.remove("visible");
+  const returnAcceuil = document.querySelector(".return");
+
+  const container = document.querySelector(".professional_experience");
+  const formation = document.querySelector(".formation");
+  const containerProfilProfessionnel = document.querySelector(
+    ".profilProfessionnel"
+  );
+
+  profilTitle.addEventListener("click", (e) => {
+    containerProfilProfessionnel.classList.add("visible");
+  });
+
+  formationTitle.addEventListener("click", (e) => {
+    formation.classList.add("visible");
   });
 
   professionnalTitle.addEventListener("click", (e) => {
-    e.preventDefault();
     container.classList.add("visible");
+  });
+
+  returnAcceuil.addEventListener("click", (e) => {
+    container.classList.remove("visible");
   });
 });
